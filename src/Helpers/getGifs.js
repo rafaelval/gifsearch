05ce35c117e2694e7c7dcb380e8b0b4590e73c52
rafael valbuena <artillero.satanas@gmail.com>
2024-06-17 11:43:13 -0500

    creada peticion y formulario de busqueda

---
+++ src/Helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=1ks79v1Yz5f77a9NOU87OhWO7hi7O0ct&q=${category}&limit=10`;
+  const res = await fetch(url);
+  const { data } = await res.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  return gifs;
+};
