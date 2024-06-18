3ebb40a8e672e0b6015c771d86b25dcba0a3b908
rafael valbuena <artillero.satanas@gmail.com>
2024-06-18 12:49:23 -0500

    listo responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AddCategory } from "./Components/AddCategory";
+import { GifGrid } from "./Components/GifGrid";
+import { AiOutlineClear } from "react-icons/ai";
 
 export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
 
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
+  const clean = () => {
+    setCategories([]);
+  };
+
+  return (
+    <div>
+      <h1 className="comic-neue-bold">Encuentra tus mejores gifs</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+      <button onClick={clean} ><AiOutlineClear /></button>
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </div>
+  );
+};
--- src/Helpers/getGifs.js
+++ src/Helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=1ks79v1Yz5f77a9NOU87OhWO7hi7O0ct&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=1ks79v1Yz5f77a9NOU87OhWO7hi7O0ct&q=${category}&limit=20`;
   const res = await fetch(url);
   const { data } = await res.json();
 
